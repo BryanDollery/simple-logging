1fe33137a604453248980248b5bc5383de39d451
Bryan Dollery <bryan.dollery@gmail.com>
2022-03-29 14:41:06 +0300

    modularise

---
+++ src/log.js
@@ -0,0 +1,24 @@
+const dump = (report, level) => {
+  let logObject = typeof report === "string" ? { msg: report } : { ...report };
+  logObject = { ...logObject, ctx, level, timestamp: new Date(), time: Date.now() };
+
+  // These two lines seem counterintuitive, but they are needed for correct error reporting.
+  const isError = report.hasOwnProperty("cause");
+  if (isError) logObject.cause = `${report.cause}`;
+
+  console.log(JSON.stringify(logObject));
+};
+
+let ctx = `pid=${process.pid}`;
+
+const log = {
+  setCtx: newCtx => (ctx = newCtx),
+  debug: report => dump(report, "DEBUG"),
+  info: report => dump(report, "INFO"),
+  error: report => dump(report, "ERROR")
+};
+
+Object.freeze(log);
+Object.seal(log);
+
+exports.log = log;
